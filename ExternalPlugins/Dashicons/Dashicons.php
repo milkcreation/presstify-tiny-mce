0bb2605caac7d7232da61469ec6269514802243d
Jordy Manner <jordy.manner@milkcreation.fr>
2018-04-25 15:01:26 +0000

    tiny-mce -- trunk -- Version initiale (non opérationnelle)

---
+++ ExternalPlugins/Dashicons/Dashicons.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace tiFy\Components\TinyMCE\ExternalPlugins\Dashicons;
+
+use tiFy\Components\TinyMCE\ExternalPlugins\AbstractGlyphs;
+
+class Dashicons extends AbstractGlyphs
+{
+    /**
+     * Nom de qualification du plugin.
+     * @var string
+     */
+    protected $name = 'dashicons';
+}
