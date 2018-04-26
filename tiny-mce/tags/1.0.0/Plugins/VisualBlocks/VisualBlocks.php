074b7526cd30fbe221e7d748d6a099a2df082b57
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2018-04-26 14:02:50 +0000

    tiny-mce -- tag 1.0.0 -- déploiement initial

---
+++ tiny-mce/tags/1.0.0/Plugins/VisualBlocks/VisualBlocks.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace tiFy\Plugins\TinyMce\Plugins\VisualBlocks;
+
+use tiFy\Plugins\TinyMce\Plugins\AbstractPlugin;
+
+class VisualBlocks extends AbstractPlugin
+{
+    /**
+     * Nom de qualification du plugin.
+     * @var string
+     */
+    protected $name = 'visualblocks';
+}
