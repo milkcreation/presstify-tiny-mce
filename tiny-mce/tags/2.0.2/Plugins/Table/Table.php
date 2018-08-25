b5879685609796b98bf912c7eeea14b4bd69e55e
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2018-08-25 18:24:26 +0000

    tiny-mce -- tag 2.0.2 -- correctif contracts tiFy v2

---
+++ tiny-mce/tags/2.0.2/Plugins/Table/Table.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace tiFy\Plugins\TinyMce\Plugins\Table;
+
+use tiFy\Plugins\TinyMce\Plugins\AbstractPlugin;
+
+class Table extends AbstractPlugin
+{
+    /**
+     * Nom de qualification du plugin.
+     * @var string
+     */
+    protected $name = 'table';
+}
