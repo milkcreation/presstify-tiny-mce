e684fc345af9ba99b4dc640a74ac718313f59587
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2018-08-28 10:34:45 +0000

    tiny-mce -- dépôt initial

---
+++ tiny-mce/branches/1.4/Plugins/JumpLine/JumpLine.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace tiFy\Plugins\TinyMce\Plugins\JumpLine;
+
+use tiFy\Plugins\TinyMce\Plugins\AbstractPlugin;
+
+class JumpLine extends AbstractPlugin
+{
+    /**
+     * Nom de qualification du plugin.
+     * @var string
+     */
+    protected $name = 'jumpline';
+
+    /**
+     * Liste des options par défaut.
+     *
+     * @return array
+     */
+    public function defaultConfig()
+    {
+        return [
+            'wp_enqueue_style' => true
+        ];
+    }
+
+    /**
+     * Initialisation globale.
+     *
+     * @return void
+     */
+    public function init()
+    {
+        parent::init();
+
+        $this->appAddAction('admin_init');
+        $this->appAddAction('wp_enqueue_scripts');
+    }
+
+    /**
+     * Initialisation de l'interface d'administration.
+     *
+     * @return void
+     */
+    public function admin_init()
+    {
+        if ((current_user_can('edit_posts') || current_user_can('edit_pages')) && get_user_option('rich_editing')) :
+            $this->appAddAction('admin_head');
+            $this->appAddAction('admin_print_styles');
+            $this->appAddFilter('mce_css');
+        endif;
+    }
+
+    /**
+     * Personnalisation de scripts de l'entête de l'interface d'administration.
+     *
+     * @return string
+     */
+    public function admin_head()
+    {
+?><script type="text/javascript">/* <![CDATA[ */var tiFyTinyMCEJumpLinel10n={'title':'<?php _e('Saut de ligne', 'tify');?>'};/* ]]> */</script><?php
+    }
+
+    /**
+     * Personnalisation de styles de l'entête de l'interface d'administration.
+     *
+     * @return string
+     */
+    public function admin_print_styles()
+    {
+?><style type="text/css">i.mce-i-jumpline::before{content:"\f474";font-family:"dashicons";}</style><?php
+    }
+
+    /**
+     * Ajout de styles dans l'éditeur tinyMCE.
+     *
+     * @return string
+     */
+    public function mce_css($mce_css)
+    {
+        return $mce_css .= ', ' . $this->appUrl() . '/editor.css';
+    }
+
+    /**
+     * Mise en file de scripts de l'interface utilisateur.
+     *
+     * @return void
+     */
+    public function wp_enqueue_scripts()
+    {
+        if ($this->getConfig('wp_enqueue_style') && $this->isActive()) :
+            wp_enqueue_style('TinyMcePluginJumpLine', $this->appUrl() . '/theme.css', [], 160625);
+        endif;
+    }
+}
