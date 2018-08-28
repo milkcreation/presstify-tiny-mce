6d0baef775b9b915cec4c0d2a29f5ac8e0f725e9
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2018-08-28 10:38:00 +0000

    tiny-mce -- branch 1.4 -- installation tiFy 1.4

--- tiny-mce/branches/1.4/TinyMce.php
+++ tiny-mce/branches/1.4/TinyMce.php
@@ -6,7 +6,7 @@
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package presstiFy
  * @namespace \tiFy\Plugins\TinyMce
- * @version 2.0.2
+ * @version 1.4.0
  */
 
 namespace tiFy\Plugins\TinyMce;
