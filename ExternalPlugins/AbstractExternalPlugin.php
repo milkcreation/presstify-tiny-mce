93ba0c9453de9002d227bd8a48df15910ef8b402
Jordy Manner <jordy.manner@milkcreation.fr>
2018-09-21 05:09:23 +0000

    tiny-mce -- branch 2.0 -- fusion 1.4 > 2.0 + réécriture

---
+++ ExternalPlugins/AbstractExternalPlugin.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace tiFy\Plugins\TinyMce\ExternalPlugins;
+
+use Illuminate\Support\Arr;
+use tiFy\Kernel\Parameters\AbstractParametersBag;
+use tiFy\Plugins\TinyMce\Contracts\ExternalPluginInterface;
+use tiFy\Plugins\TinyMce\TinyMce;
+
+abstract class AbstractExternalPlugin extends AbstractParametersBag implements ExternalPluginInterface
+{
+    /**
+     * Nom de qualification du plugin.
+     * @var string
+     */
+    protected $name = '';
+
+    /**
+     * Instance du controleur principal.
+     * @var TinyMce
+     */
+    protected $tinyMce;
+
+    /**
+     * CONSTRUCTEUR.
+     *
+     * @param string Nom de qualification du plugin.
+     * @param array $attrs Liste des attributs de configuration.
+     * @param TinyMce $tinyMce Instance du controleur principal.
+     *
+     * @return void
+     */
+    public function __construct($name, $attrs = [], TinyMce $tinyMce)
+    {
+        $this->name = $name;
+        $this->tinyMce = $tinyMce;
+
+        parent::__construct($attrs);
+
+        $this->tinyMce()->setExternalPlugin($this);
+
+        add_action(
+            'after_setup_theme',
+            function () use ($attrs) {
+                $this->parse($attrs);
+            }
+        );
+
+        $this->boot();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getUrl()
+    {
+        return class_info($this)->getUrl() . '/plugin.js';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isActive()
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function parse($attrs = [])
+    {
+        parent::parse($attrs);
+
+        if ($mce_init = $this->get('mce_init', [])) :
+            $this->tinyMce()->setAdditionnalConfig($mce_init);
+        endif;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function tinyMce()
+    {
+        return $this->tinyMce;
+    }
+}
--- ExternalPlugins/Template/Template.php
+++ ExternalPlugins/Template/Template.php
@@ -1,26 +1,28 @@
 <?php
 
-namespace tiFy\Plugins\TinyMce\Plugins\Template;
+namespace tiFy\Plugins\TinyMce\ExternalPlugins\Template;
 
-use tiFy\Plugins\TinyMce\Plugins\AbstractPlugin;
+use tiFy\Plugins\TinyMce\ExternalPlugins\AbstractExternalPlugin;
 
-class Template extends AbstractPlugin
+class Template extends AbstractExternalPlugin
 {
     /**
-     * Nom de qualification du plugin.
-     * @var string
+     * {@inheritdoc}
      */
-    protected $name = 'template';
+    public function boot()
+    {
+        add_filter('mce_css', [$this, 'mce_css']);
+        add_action('wp_enqueue_scripts', [$this, 'wp_enqueue_scripts']);
+        add_action('wp_ajax_tinymce_plugin_template', [$this, 'wp_ajax']);
+    }
 
     /**
-     * Liste des attributs de configuration par défaut.
-     *
-     * @return array
+     * {@inheritdoc}
      */
-    public function defaultConfig()
+    public function defaults()
     {
         return [
-            'wp_enqueue_style' => true,
+            'wp_enqueue_scripts' => true,
             'mce_init'         => [
                 'templates' => \add_query_arg(
                     [
@@ -34,17 +36,13 @@ class Template extends AbstractPlugin
     }
 
     /**
-     * Initialisation globale de Wordpress.
+     * Ajout de styles dans l'éditeur tinyMCE.
      *
-     * @return void
+     * @return string
      */
-    public function init()
+    public function mce_css($mce_css)
     {
-        parent::init();
-
-        $this->appAddFilter('mce_css');
-        $this->appAddAction('wp_enqueue_scripts');
-        $this->appAddAction('wp_ajax_tinymce_plugin_template', [$this, 'wp_ajax']);
+        return $mce_css .= ', ' . class_info($this)->getUrl() . '/editor.css';
     }
 
     /**
@@ -62,69 +60,61 @@ class Template extends AbstractPlugin
 
         header('Content-Type: application/x-javascript; charset=UTF-8');
 
+        $url = class_info($this)->getUrl();
+
         echo json_encode([
             [
                 "title"       => "2 Colonnes : 1/4, 3/4",
                 "description" => "1 colonne d'1/4 et l'autre de 3/4",
-                "url"         => $this->appUrl() . "/templates/2cols_0.25-0.75.htm"
+                "url"         => $url . "/templates/2cols_0.25-0.75.htm"
             ],
             [
                 "title"       => "2 Colonnes : 1/3, 2/3",
                 "description" => "1 colonne d'1/3 et l'autre de 2/3",
-                "url"         => $this->appUrl() . "/templates/2cols_0.33-0.66.htm"
+                "url"         => $url . "/templates/2cols_0.33-0.66.htm"
             ],
             [
                 "title"       => "2 Colonnes : 1/2, 1/2",
                 "description" => "1 colonnes d'1/2 et l'autre d'1/2",
-                "url"         => $this->appUrl() . "/templates/2cols_0.5-0.5.htm"
+                "url"         => $url . "/templates/2cols_0.5-0.5.htm"
             ],
             [
                 "title"       => "2 Colonnes : 2/3, 1/3",
                 "description" => "1 colonne de 2/3 et l'autre d'1/3",
-                "url"         => $this->appUrl() . "/templates/2cols_0.66-0.33.htm"
+                "url"         => $url . "/templates/2cols_0.66-0.33.htm"
             ],
             [
                 "title"       => "2 Colonnes : 3/4, 1/4",
                 "description" => "1 colonne de 3/4 et l'autre d'1/4",
-                "url"         => $this->appUrl() . "/templates/2cols_0.75-0.25.htm"
+                "url"         => $url . "/templates/2cols_0.75-0.25.htm"
             ],
             [
                 "title"       => "3 Colonnes : 1/4, 1/4, 1/2",
                 "description" => "1 colonne d'1/4, une d'1/4 et une d'1/2",
-                "url"         => $this->appUrl() . "/templates/3cols_0.25-0.25-0.5.htm"
+                "url"         => $url . "/templates/3cols_0.25-0.25-0.5.htm"
             ],
             [
                 "title"       => "3 Colonnes : 1/4, 1/2, 1/4",
                 "description" => "1 colonne d'1/4, une d'1/2 et une d'1/4",
-                "url"         => $this->appUrl() . "/templates/3cols_0.25-0.5-0.25.htm"
+                "url"         => $url . "/templates/3cols_0.25-0.5-0.25.htm"
             ],
             [
                 "title"       => "3 Colonnes : 1/3, 1/3, 1/3",
                 "description" => "1 colonne d'1/3, une d'1/3 et une d'1/3",
-                "url"         => $this->appUrl() . "/templates/3cols_0.33-0.33-0.33.htm"
+                "url"         => $url . "/templates/3cols_0.33-0.33-0.33.htm"
             ],
             [
                 "title"       => "3 Colonnes : 1/2, 1/4, 1/4",
                 "description" => "1 colonne d'1/2, une d'1/4 et une d'1/4",
-                "url"         => $this->appUrl() . "/templates/3cols_0.5-0.25-0.25.htm"
+                "url"         => $url . "/templates/3cols_0.5-0.25-0.25.htm"
             ],
             [
                 "title"       => "4 Colonnes : 1/4, 1/4, 1/4, 1/4",
                 "description" => "1 colonnes d'1/4, une d'1/4, une d'1/4 et une d'1/4",
-                "url"         => $this->appUrl() . "/templates/4cols_0.25-0.25-0.25-0.25.htm"
+                "url"         => $url . "/templates/4cols_0.25-0.25-0.25-0.25.htm"
             ]
         ]);
         exit;
-    }
-
-    /**
-     * Ajout de styles dans l'éditeur tinyMCE.
-     *
-     * @return string
-     */
-    public function mce_css($mce_css)
-    {
-        return $mce_css .= ', ' . $this->appUrl() . '/editor.css';
     }
 
     /**
@@ -134,8 +124,8 @@ class Template extends AbstractPlugin
      */
     public function wp_enqueue_scripts()
     {
-        if ($this->getConfig('wp_enqueue_style') && $this->isActive()) :
-            wp_enqueue_style('TinyMcePluginTemplate', $this->appUrl() . '/theme.css', [], 150317);
+        if ($this->get('wp_enqueue_scripts') && $this->isActive()) :
+            wp_enqueue_style('TinyMceExternalPluginsTemplate', class_info($this)->getUrl() . '/theme.css', [], 150317);
         endif;
     }
 }
