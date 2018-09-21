7b560efe9d944544a80179e7d1b78131360ca00a
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2018-09-21 05:21:24 +0000

    tiny-mce -- tag 2.0.3 -- fusion 1.4 > 2.0 + réécriture

---
+++ tiny-mce/tags/2.0.3/ExternalPlugins/Ownglyphs/Ownglyphs.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace tiFy\Plugins\TinyMce\ExternalPlugins\OwnGlyphs;
+
+use tiFy\Plugins\TinyMce\ExternalPlugins\AbstractExternalPluginGlyph;
+
+class Ownglyphs extends AbstractExternalPluginGlyph
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        add_action('wp_ajax_tify_tinymce_external_plugins_ownglyphs', [$this, 'wp_ajax']);
+    }
+
+    /**
+     * Mise en file de scripts de l'interface d'administration.
+     *
+     * @return void
+     */
+    public function admin_enqueue_scripts()
+    {
+        if ($this->get('admin_enqueue_scripts')) :
+            wp_enqueue_style($this->get('hookname'));
+        endif;
+
+        wp_enqueue_style('tiFyTinyMceExternalPlugins' . class_info($this)->getShortName());
+
+        assets()->addInlineJs(
+            "var glyphs=" . wp_json_encode($this->parseGlyphs()) . "," .
+            "tinymceOwnGlyphsl10n={'title':'{$this->get('title')}'};",
+            'admin'
+        );
+
+        assets()->addInlineCss(
+            "i.mce-i-ownglyphs::before{content:'{$this->glyphs[$this->get('button')]}';}" .
+            "i.mce-i-ownglyphs::before,.mce-grid a.ownglyphs{font-family:'{$this->get('font-family')}'!important;}",
+            'admin'
+        );
+    }
+
+    /**
+     * Ajout de styles dans l'éditeur tinyMCE.
+     *
+     * @return string
+     */
+    public function mce_css($mce_css)
+    {
+        if ($this->get('editor_enqueue_scripts')) :
+            $mce_css .= ', ' . $this->get('css');
+        endif;
+
+        return $mce_css .= ', ' . class_info($this)->getUrl() . '/editor.css, ' .
+            admin_url(
+                'admin-ajax.php?action=tify_tinymce_external_plugins_ownglyphs&bogus=' . current_time('timestamp')
+            );
+    }
+
+    /**
+     * Action Ajax.
+     *
+     * @return string
+     */
+    public function wp_ajax()
+    {
+        header("Content-type: text/css");
+        echo '.ownglyphs{font-family:' . $this->get('font-family') . ';}';
+        exit;
+    }
+}
