e9a5d7c1b0460ec77e65edaa569b55624ab2d759
Jordy Manner <jordy.manner@milkcreation.fr>
2018-12-04 09:43:53 +0000

    tiny-mce -- trunk -- Correction label config

---
+++ ExternalPlugins/Fontawesome/Fontawesome.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace tiFy\Plugins\TinyMce\ExternalPlugins\FontAwesome;
+
+use tiFy\Plugins\TinyMce\ExternalPlugins\AbstractExternalPluginGlyph;
+
+class Fontawesome extends AbstractExternalPluginGlyph
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function defaults()
+    {
+        return [
+            'hookname'               => 'font-awesome',
+            'css'                    => home_url('/vendor/fortawesome/font-awesome/css/font-awesome.css'),
+            'admin_enqueue_scripts'  => true,
+            'editor_enqueue_scripts' => true,
+            'wp_enqueue_scripts'     => true,
+            'dependencies'           => [],
+            'prefix'                 => 'fa-',
+            'font-family'            => 'fontAwesome',
+            'button'                 => 'flag',
+            'title'                  => __('Police de caractères fontAwesome', 'tify'),
+            'cols'                   => 32,
+        ];
+    }
+
+    /**
+     * Mise en file de scripts de l'interface d'administration.
+     *
+     * @return void
+     */
+    public function admin_enqueue_scripts()
+    {
+        if ($this->get('admin_enqueue_scripts')) :
+            wp_enqueue_style($this->get('hookname'));
+        endif;
+
+        wp_enqueue_style('tiFyTinyMceExternalPlugins' . class_info($this)->getShortName());
+
+        assets()->addInlineJs(
+            "var fontAwesomeChars=" . wp_json_encode($this->parseGlyphs()) .
+            ",tinymceFontAwesomel10n={'title':'{$this->get('title')}'};",
+            'admin'
+        );
+
+        assets()->addInlineCss(
+            "i.mce-i-fontawesome:before{content:'{$this->glyphs[$this->get('button')]}';}" .
+            "i.mce-i-fontawesome:before,.mce-grid a.fontawesome{font-family:'{$this->get('font-family')}'!important;}",
+            'admin'
+        );
+    }
+
+    /**
+     * Personnalisation de l'entête de l'interface utilisateur.
+     *
+     * @return void
+     */
+    public function wp_head()
+    {
+        assets()->addInlineCss(".fontawesome{font-family:'{$this->get('font-family')}';}");
+    }
+}
