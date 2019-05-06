45e4493ec673e44a33764b091e5d94e2f3b444e3
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-05-06 09:17:16 +0000

    tiny-mce -- tag 2.0.11 -- réécriture asset adaptation

---
+++ tiny-mce/tags/2.0.11/ExternalPlugins/Jumpline/Jumpline.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace tiFy\Plugins\TinyMce\ExternalPlugins\JumpLine;
+
+use tiFy\Plugins\TinyMce\ExternalPlugins\AbstractExternalPlugin;
+
+class JumpLine extends AbstractExternalPlugin
+{
+    /**
+     * @inheritdoc
+     */
+    public function boot()
+    {
+        add_action('admin_init', [$this, 'admin_init']);
+        add_action('wp_enqueue_scripts', [$this, 'wp_enqueue_scripts']);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function defaults()
+    {
+        return [
+            'wp_enqueue_scripts' => true
+        ];
+    }
+
+    /**
+     * Initialisation de l'interface d'administration.
+     *
+     * @return void
+     */
+    public function admin_init()
+    {
+        if ((current_user_can('edit_posts') || current_user_can('edit_pages')) && get_user_option('rich_editing')) :
+            add_action('admin_enqueue_scripts', [$this, 'admin_enqueue_scripts']);
+            add_filter('mce_css', [$this, 'mce_css']);
+        endif;
+    }
+
+    /**
+     * Mise en file de scripts de l'interface d'administration.
+     *
+     * @return void
+     */
+    public function admin_enqueue_scripts()
+    {
+        asset()->setInlineJs("let tiFyTinyMCEJumpLinel10n={'title':'" . __('Saut de ligne', 'tify') . "'};", true);
+
+        asset()->setInlineCss("i.mce-i-jumpline::before{content:'\\f474';font-family:'dashicons';}");
+    }
+
+    /**
+     * Ajout de styles dans l'éditeur tinyMCE.
+     *
+     * @param string $mce_css Liste des url vers les feuilles de styles associées à tinyMCE.
+     *
+     * @return string
+     */
+    public function mce_css($mce_css)
+    {
+        return $mce_css . ', ' . $this->tinyMce()->getPluginAssetsUrl($this->getName()) . '/css/editor.css';
+    }
+
+    /**
+     * Mise en file de scripts de l'interface utilisateur.
+     *
+     * @return void
+     */
+    public function wp_enqueue_scripts()
+    {
+        if ($this->get('wp_enqueue_scripts') && $this->isActive()) :
+            wp_enqueue_style(
+                'TinyMceExternalPluginsJumpLine',
+                $this->tinyMce()->getPluginAssetsUrl($this->getName()) . '/css/styles.css',
+                [],
+                160625
+            );
+        endif;
+    }
+}
