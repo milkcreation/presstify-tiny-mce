25d758c3e2c9ff6dab7e3a3e3606d8097d161aac
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-12-11 13:56:14 +0000

    tiny-mce -- tag 1.4.17 -- Version bridgée

---
+++ tiny-mce/tags/1.4.17/ExternalPlugins/Table/Table.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace tiFy\Plugins\TinyMce\ExternalPlugins\Table;
+
+use tiFy\Plugins\TinyMce\ExternalPlugins\AbstractExternalPlugin;
+
+class Table extends AbstractExternalPlugin
+{
+
+}
