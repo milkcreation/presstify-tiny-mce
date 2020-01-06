3dcba5f02e469f50721bfbb0d607f23573692457
Jordy Manner <jordy.manner@milkcreation.fr>
2020-01-06 06:28:09 +0000

    tiny-mce -- tag 2.0.19 -- passe de réécriture

--- ExternalPlugins/Fontawesome/Fontawesome.php
+++ ExternalPlugins/Fontawesome/Fontawesome.php
@@ -7,13 +7,13 @@ use tiFy\Plugins\TinyMce\ExternalPlugins\AbstractExternalPluginGlyph;
 class Fontawesome extends AbstractExternalPluginGlyph
 {
     /**
-     * {@inheritdoc}
+     * @inheritDoc
      */
     public function defaults()
     {
-        return [
+        return array_merge(parent::defaults(), [
             'hookname'               => 'font-awesome',
-            'css'                    => home_url('/vendor/fortawesome/font-awesome/css/font-awesome.css'),
+            'path'                    => '/vendor/fortawesome/font-awesome/css/font-awesome.css',
             'admin_enqueue_scripts'  => true,
             'editor_enqueue_scripts' => true,
             'wp_enqueue_scripts'     => true,
@@ -23,7 +23,7 @@ class Fontawesome extends AbstractExternalPluginGlyph
             'button'                 => 'flag',
             'title'                  => __('Police de caractères fontAwesome', 'tify'),
             'cols'                   => 32,
-        ];
+        ]);
     }
 
     /**
@@ -33,22 +33,21 @@ class Fontawesome extends AbstractExternalPluginGlyph
      */
     public function admin_enqueue_scripts()
     {
-        if ($this->get('admin_enqueue_scripts')) :
+        if ($this->get('admin_enqueue_scripts')) {
             wp_enqueue_style($this->get('hookname'));
-        endif;
+        }
 
         wp_enqueue_style('tiFyTinyMceExternalPlugins' . class_info($this)->getShortName());
 
-        assets()->addInlineJs(
-            "var fontAwesomeChars=" . wp_json_encode($this->parseGlyphs()) .
+        asset()->setInlineJs(
+            "let fontAwesomeChars=" . wp_json_encode($this->parseGlyphs()) .
             ",tinymceFontAwesomel10n={'title':'{$this->get('title')}'};",
-            'admin'
+            true
         );
 
-        assets()->addInlineCss(
+        asset()->setInlineCss(
             "i.mce-i-fontawesome:before{content:'{$this->glyphs[$this->get('button')]}';}" .
-            "i.mce-i-fontawesome:before,.mce-grid a.fontawesome{font-family:'{$this->get('font-family')}'!important;}",
-            'admin'
+            "i.mce-i-fontawesome:before,.mce-grid a.fontawesome{font-family:'{$this->get('font-family')}'!important;}"
         );
     }
 
@@ -59,6 +58,6 @@ class Fontawesome extends AbstractExternalPluginGlyph
      */
     public function wp_head()
     {
-        assets()->addInlineCss(".fontawesome{font-family:'{$this->get('font-family')}';}");
+        asset()->setInlineCss(".fontawesome{font-family:'{$this->get('font-family')}';}");
     }
 }
